e6b59a18815a670d2cacbb1c255529c4e0d74850
Rakshit Raj~ <rakshit.raj.cg@gmail.com>
2025-09-15 13:02:28 +0530

    q upto 12

---
+++ C language/assign5-q12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {              
+        for (int s = 1 ; s <= n - i; s++) {     
+            printf("  ");                      
+        }
+        for (int j = 0; j < i; j++) {          
+            printf("%c ", 'A' + j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
